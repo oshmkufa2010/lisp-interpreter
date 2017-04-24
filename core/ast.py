5126b293086f58ca1a345c0ece610b81804862ed
cailin <cailinunix@gmail.com>
2017-04-24 23:21:09 +0800

    reactor some code

--- core/ast.py
+++ core/ast.py
@@ -2,6 +2,7 @@ from abc import ABCMeta, abstractmethod
 import collections
 from parser import Type as TokenType
 
+
 class AST(object):
     __metaclass__ = ABCMeta
 
@@ -19,61 +20,137 @@ class AST(object):
 
 
 class AstNodeType(object):
-    PLUS = 0
-    MINUS = 1
-    TIMES = 2
-    DIVIDE = 3
-    VAR = 4
-    LET = 5
-    LAMBDA = 6
-    CALL = 7
-    ARG_TUPLE = 8
-    VALUE_TUPLE = 9
-    BIND_PAIR = 10
-    BIND_PAIR_TUPLE = 11
+    NUM = 'NUM'
+    PLUS = 'PLUS'
+    MINUS = 'MINUS'
+    TIMES = 'TIMES'
+    DIVIDE = 'DIVIDE'
+    LET = 'LET'
+    LAMBDA = 'LAMBDA'
+    VAR = 'VAR'
+    EQ = 'EQ'
+    NEQ = 'NEQ'
+    LT = 'LT'
+    MT = 'MT'
+    LTE = 'LTE'
+    MTE = 'MTE'
+    IF = 'IF'
+    CALL = 'CALL'
+    ARG_TUPLE = 'ARG_TUPLE'
+    PARA_TUPLE = 'PARA_TUPLE'
+    BIND_PAIR_TUPLE = 'BIND_PAIR_TUPLE'
+
+    @classmethod
+    def get_type_by_name(cls, name):
+        return getattr(cls, name)
 
 
 class AstNode(object):
-    def __init__(self, token_type, token_value=None):
-        pass
+    def __init__(self, node_type, node_value=None):
+        self.type = token_type
+        self.value = node_value
+
+
+class AstNodeCreator(object):
+    def __init__(self, cls):
+        self.cls = cls
+
+    def __getattr__(self, node_name):
+        node_type = getattr(AstNodeType, node_name)
+        return self.cls(node_type)
 
 
 class SExpAST(AST):
     def __init__(self, node, children=None):
         self.__node = node
-        self.__children = children or []
+        self.__children = children or ()
 
     @classmethod
     def from_sexp(cls, sexp):
+
+        # number or variable
         if isinstance(sexp, Token):
-            pass
+            if sexp.type == TokenType.NUM:
+                return cls(AstNode(AstNodeType.NUM), sexp.value)
+            elif sexp.type == TokenType.VAR:
+                return cls(AstNode(AstNodeType.VAR), sexp.value)
+
         op = sexp[0]
+
         # call
         if isinstance(op, list) or op.type == TokenType.VAR:
-            args_node = AstNode(AstNodeType.ARG_TUPLE, None)
-            args_children = [cls.from_sexp(child) for child in sexp[1:]]
-            args_ast = cls(args_node, args_children)
+            args = tuple(cls.from_sexp(child) for child in sexp[1:])
+            args_node = AstNode(AstNodeType.ARG_TUPLE, args)
+            args_ast = cls(args_node)
 
             fn_ast = cls.from_sexp(op)
 
-            children = [fn_ast, args_ast]
-
             call_node = AstNode(AstNodeType.CALL, None)
 
-            return cls(call_node, children)
+            return cls(call_node, (fn_ast, args_ast))
+
+        # lambda
+        elif op.type == TokenType.LAMBDA:
+            paras = tuple(para.value for para in sexp[1])
+            paras_node = AstNode(AstNodeType.PARA_TUPLE, paras)
+
+            paras_ast = cls(paras_node)
+
+            body_ast = cls.from_sexp(sexp[2])
+
+            lambda_node = AstNode(AstNodeType.LAMBDA, None)
+
+            return cls(lambda_node, (paras_ast, body_ast))
+
+        # let
+        elif op.type == TokenType.LET:
+            bind_pairs = tuple((pair[0].value, cls.from_sexp(pair[1])) for pair in sexp[1])
+            bind_pairs_node = AstNode(AstNodeType.BIND_PAIR_TUPLE, bind_pairs)
+            bind_pairs_ast = cls(bind_pairs_node)
+
+            body_ast = cls.from_sexp(sexp[2])
+
+            let_node = AstNode(AstNodeType.LET, None)
+
+            return cls(let_node, (bind_pairs_ast, body_ast))
+
+        # if
+        elif op.type == TokenType.IF:
+            children = tuple(cls.from_sexp(exp) for exp in sexp[1:])
+            if_node = AstNode(AstNodeType.IF, None)
+            return cls(if_node, children)
+
+        elif op.type in (TokenType.EQ,
+                         TokenType.NEQ,
+                         TokenType.LT,
+                         TokenType.LTE,
+                         TokenType.MT,
+                         TokenType.MTE
+                         TokenType.PLUS,
+                         TokenType.MINUS,
+                         TokenType.TIMES,
+                         TokenType.DIVIDE):
+            node = AstNode(AstNodeType.get_type_by_name(op.type), None)
+            children = tuple(cls.from_sexp(exp) for exp in sexp[1:])
+            return cls(node, children)
 
         else:
-            pass
+            raise SyntaxError
 
     def left(self):
-        return self.__sexp[1]
+        try:
+            return self.__children[0]
+        except IndexError:
+            return ()
 
     def right(self):
-        return self.__sexp[2]
+        try:
+            return self.__children[1]
+        except IndexError:
+            return ()
 
     def children(self):
-
-        return self.__sexp[1:]
+        return self.__children
 
     def __getattr__(self, attr):
         return getattr(self.__node, attr)
--- core/interpreter.py
+++ core/interpreter.py
@@ -4,7 +4,8 @@
 from contextlib import contextmanager
 import copy
 from errors import UnBoundError
-from parser import parse, Token, Type
+from parser import parse, Token
+from ast import AstNodeType as Type
 
 
 class Env(object):
@@ -65,29 +66,30 @@ class Closure(object):
 
 
 class Interpreter(object):
-    def interpret(self, node, env):
-        if node.type == Type.NUM:
-            return self.interpret_num(node, env)
+    def interpret(self, ast, env=None):
+        if env is None:
+            env = Env()
+        method_name = 'interpret_' + ast.type.lower()
+        method = getattr(self, method_name)
+        if method:
+            return method(ast, env)
+        else:
+            if ast.type in (Type.PLUS,):
+                pass
 
-        elif node.type == Type.VAR:
-            return self.interpret_var(node, value)
 
-        elif node.type == Type.LAMBDA:
-            return self.interpret_lambda(node, env)
+    def interpret_num(self, ast, env):
+        return ast.value
 
-
-    def interpret_num(self, node, env):
-        return node.value
-
-    def interpret_var(self, node, env):
-        bound = env.look_up(node.value)
+    def interpret_var(self, ast, env):
+        bound = env.look_up(ast.value)
         if bound is None:
             raise UnBoundError
         return bound
 
-    def interpret_call(self, node, env):
-        closure = self.interpret(node, env)
-        arg_values = [self.interpret(arg, env) for arg in node.children]
+    def interpret_call(self, ast, env):
+        closure = self.interpret(ast.children[0], env)
+        arg_values = [self.interpret(arg, env) for arg in ast.children]
 
         env_save = closure.env
         expr_save = closure.expr
@@ -99,54 +101,57 @@ class Interpreter(object):
         with env_save.ext_env(scope) as new_env:
             return self.interpret(expr_save, new_nev)
 
-    def _interpret_arg_tuple(self, node):
-        return [node.value] + [child.value for child in node.children]
+    def inpterpret_arg_tuple(self, ast, env):
+        return tuple(self.interpret(arg, env) for arg in ast.value)
 
-    def interpret_bind(self, node, env):
-        arg_value_pairs = self._interpret_bind_pair(node.children[0], env)
+    def interpret_para_tuple(self, ast, env):
+        return ast.value
+
+    def interpret_bind_pair_tuple(self, ast, env):
+        return tuple((pair[0], self.interpret(pair[1], env)) for pair in ast.value)
+
+
+    def interpret_let(self, ast, env):
+        arg_value_pairs = self.interpret(ast.children[0], env)
         scope = Scope.make_scope(arg_value_pairs)
         with env.ext_env(scope) as new_nev:
-            return interpret(node.children[1], new_nev)
+            return self.interpret(ast.children[1], new_nev)
 
-    def _interpret_bind_pair(self, node, env):
-        arg = node.value
-        val = self.interpret(node.children[0], env)
-        return (arg, val)
 
-    def interpret_lambda(self, node, env):
-        return Closure(node, env)
+    def interpret_lambda(self, ast, env):
+        return Closure(ast.children, env)
 
-    def interpret_if(self, node, env):
-        cond_node, then_node, else_node = node.children
-        if self.interpret(cond_node, env):
-            return self.interpret(then_node, env)
+    def interpret_if(self, ast, env):
+        cond_ast, then_ast, else_ast = ast.children
+        if self.interpret(cond_ast, env):
+            return self.interpret(then_ast, env)
         else:
-            return self.interpret(else_node, env)
+            return self.interpret(else_ast, env)
 
-    def interpret_condition(self, node, env):
-        left, value, right_value = (self.interpret(child, env) for child in node.children)
-        if node.type == Type.EQ:
+    def interpret_condition(self, ast, env):
+        left, value, right_value = (self.interpret(child, env) for child in ast.children)
+        if ast.type == Type.EQ:
             return left_value == right_value
-        elif node.type == Type.NEQ:
+        elif ast.type == Type.NEQ:
             return left_value != right_value
-        elif node.type == Type.LT:
+        elif ast.type == Type.LT:
             return left_value < right_value
-        elif node.type == Type.LTE:
+        elif ast.type == Type.LTE:
             return left_value <= right_value
-        elif node.type == Type.MT:
+        elif ast.type == Type.MT:
             return left_value > right_value
-        elif node.type == Type.MTE:
+        elif ast.type == Type.MTE:
             return left_value >= right_value
 
-    def interpret_arithmetic(self, node, env):
-        values = [self.interpret(child, env) for child in node.children]
+    def interpret_arithmetic(self, ast, env):
+        values = [self.interpret(child, env) for child in ast.children]
         if len(values) >= 2:
-            if node.type == Type.PLUS:
+            if ast.type == Type.PLUS:
                 return reduce(lambda x, y: x+y, values)
-            elif node.type == Type.TIMES:
+            elif ast.type == Type.TIMES:
                 return reduce(lambda x, y: x*y, values, 1)
             elif len(values) == 2:
-                if node.type == Type.MINUS:
+                if ast.type == Type.MINUS:
                     return values[0] - values[1]
                 else:
                     return values[0] / values[1]
@@ -154,7 +159,7 @@ class Interpreter(object):
                 raise SyntaxError
 
         elif len(values) == 1:
-            if node.type == Type.MINUS:
+            if ast.type == Type.MINUS:
                 return values[0] * (-1)
             else:
                 raise SyntaxError
